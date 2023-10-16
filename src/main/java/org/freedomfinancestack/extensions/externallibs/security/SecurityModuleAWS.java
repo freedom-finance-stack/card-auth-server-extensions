6151605b238d406814310bb298b38fe5f297b6a7
Ankit Choudhary <ankit.choudhary@razorpay.com>
2023-10-16 16:49:24 +0530

    Upgrading jakarta plugin for java 17

--- src/main/java/org/freedomfinancestack/extensions/externallibs/security/SecurityModuleAWS.java
+++ src/main/java/org/freedomfinancestack/extensions/externallibs/security/SecurityModuleAWS.java
@@ -1,7 +1,6 @@
 package org.freedomfinancestack.extensions.externallibs.security;
 
-import javax.servlet.http.HttpServletRequest;
-
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
