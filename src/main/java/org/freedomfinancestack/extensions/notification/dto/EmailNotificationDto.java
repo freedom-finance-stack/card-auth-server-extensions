776cf4e15a2d0bf7ba9752465a096d2045e402a0
Jaydeep Radadiya <jaydeep.radadiya.9041@gmail.com>
2023-10-30 13:58:45 +0530

    builder added for dtos

--- src/main/java/org/freedomfinancestack/extensions/notification/dto/EmailNotificationDto.java
+++ src/main/java/org/freedomfinancestack/extensions/notification/dto/EmailNotificationDto.java
@@ -3,11 +3,13 @@ package org.freedomfinancestack.extensions.notification.dto;
 import java.util.List;
 import java.util.Map;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class EmailNotificationDto extends NotificationDto {
     String templateName;
     Map<String, String> templateData;
--- src/main/java/org/freedomfinancestack/extensions/notification/dto/SMSNotificationDto.java
+++ src/main/java/org/freedomfinancestack/extensions/notification/dto/SMSNotificationDto.java
@@ -2,11 +2,13 @@ package org.freedomfinancestack.extensions.notification.dto;
 
 import java.util.List;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class SMSNotificationDto extends NotificationDto {
     int priority;
     String message;
