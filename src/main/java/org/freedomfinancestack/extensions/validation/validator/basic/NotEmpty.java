087c831e4c6dc5d60ee67af3d20faeb20b086c24
Ankit Choudhary <ankit.choudhary@razorpay.com>
2023-11-15 15:08:20 +0530

    Adding not empty validation

---
+++ src/main/java/org/freedomfinancestack/extensions/validation/validator/basic/NotEmpty.java
@@ -0,0 +1,27 @@
+package org.freedomfinancestack.extensions.validation.validator.basic;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import org.freedomfinancestack.extensions.validation.exception.ValidationErrorCode;
+import org.freedomfinancestack.extensions.validation.exception.ValidationException;
+import org.freedomfinancestack.extensions.validation.validator.Validator;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class NotEmpty<T> implements Validator<T> {
+
+    public static <T> NotEmpty<T> notEmpty() {
+        return new NotEmpty<T>();
+    }
+
+    @Override
+    public void validate(T value) throws ValidationException {
+        if (null == value) {
+            throw new ValidationException(
+                    ValidationErrorCode.REQUIRED_DATA_ELEMENT_MISSING, "Invalid value");
+        } else if ("".equals(value.toString())) {
+            throw new ValidationException(
+                    ValidationErrorCode.REQUIRED_DATA_ELEMENT_MISSING, "Invalid value");
+        }
+    }
+}
