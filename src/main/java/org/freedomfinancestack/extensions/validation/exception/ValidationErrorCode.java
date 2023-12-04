34f82d4ab5304900875d99bfde1af60ce71873d1
Jaydeep Radadiya <jaydeep.radadiya.9041@gmail.com>
2023-12-04 16:34:14 +0530

    isEqual validator added

--- src/main/java/org/freedomfinancestack/extensions/validation/exception/ValidationErrorCode.java
+++ src/main/java/org/freedomfinancestack/extensions/validation/exception/ValidationErrorCode.java
@@ -7,7 +7,8 @@ public enum ValidationErrorCode {
     INVALID_FORMAT("1000", "Format of one or more elements is invalid"),
     INVALID_FORMAT_LENGTH("1001", "Invalid Format - Length"),
     INVALID_FORMAT_VALUE("1002", "Invalid Format - Value"),
-    REQUIRED_DATA_ELEMENT_MISSING("1003", "A element required is missing from the request.");
+    REQUIRED_DATA_ELEMENT_MISSING("1003", "A element required is missing from the request."),
+    NOT_EQUAL("1004", "A element is equal to the given value.");
 
     private final String errorCode;
     private final String defaultErrorMessage;
